a742f6fd5ba7c12b5f674e4f99f8f6a87a66aab3
umt2846 <mboran16@ku.edu.tr>
2020-08-28 23:05:21 +0300

    Added documentation files

---
+++ doc/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['led_5fservice_5f1000_2',['LED_Service_1000',['../objects_8c.html#a3c2d7b77e02ee3fc3faf97a9a270da65',1,'objects.c']]],
+  ['led_5fservice_5ft_3',['LED_Service_t',['../struct_l_e_d___service__t.html',1,'']]]
+];
---
+++ doc/html/search/all_4.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['obj_5farg_5ft_6',['Obj_arg_t',['../struct_obj__arg__t.html',1,'']]],
+  ['obj_5fattr_5ft_7',['obj_attr_t',['../objects_8h.html#aecee1a6128596e65caaee0a427d64f75',1,'objects.h']]],
+  ['obj_5fbytes_5ft_8',['Obj_bytes_t',['../union_obj__bytes__t.html',1,'']]],
+  ['obj_5fcontrol_5ft_9',['Obj_control_t',['../struct_obj__control__t.html',1,'']]],
+  ['obj_5ferror_5fcode_10',['Obj_Error_Code',['../object__control_8h.html#ab3ea050c4d4928094c041b90e79ac2de',1,'object_control.h']]],
+  ['obj_5fgetuint16_11',['Obj_getUint16',['../object__control_8c.html#a8a91e7a3c6dc76d2dc71c7610b6be822',1,'Obj_getUint16(const uint8_t data[]):&#160;object_control.c'],['../object__control_8h.html#a8a91e7a3c6dc76d2dc71c7610b6be822',1,'Obj_getUint16(const uint8_t data[]):&#160;object_control.c']]],
+  ['obj_5fgetuint32_12',['Obj_getUint32',['../object__control_8c.html#afcc1f766bf248e0a886fee498d962bb4',1,'Obj_getUint32(const uint8_t data[]):&#160;object_control.c'],['../object__control_8h.html#afcc1f766bf248e0a886fee498d962bb4',1,'Obj_getUint32(const uint8_t data[]):&#160;object_control.c']]],
+  ['obj_5findex_5ft_13',['Obj_index_t',['../struct_obj__index__t.html',1,'']]],
+  ['obj_5fread_14',['obj_read',['../object__control_8c.html#a034038a9303c12c9f8cb645e9deea86e',1,'obj_read(Obj_control_t *control, uint16_t index, uint8_t subIndex):&#160;object_control.c'],['../object__control_8h.html#a034038a9303c12c9f8cb645e9deea86e',1,'obj_read(Obj_control_t *control, uint16_t index, uint8_t subIndex):&#160;object_control.c']]],
+  ['obj_5fsetuint16_15',['Obj_setUint16',['../object__control_8c.html#ac0866beeb88632b2f94dc67a8832dd52',1,'Obj_setUint16(uint8_t data[], const uint16_t value):&#160;object_control.c'],['../object__control_8h.html#ac0866beeb88632b2f94dc67a8832dd52',1,'Obj_setUint16(uint8_t data[], const uint16_t value):&#160;object_control.c']]],
+  ['obj_5fsetuint32_16',['Obj_setUint32',['../object__control_8c.html#a867711d7a2c822759ad035b1572f70ab',1,'Obj_setUint32(uint8_t data[], const uint32_t value):&#160;object_control.c'],['../object__control_8h.html#a867711d7a2c822759ad035b1572f70ab',1,'Obj_setUint32(uint8_t data[], const uint32_t value):&#160;object_control.c']]],
+  ['obj_5fsubindex_5ft_17',['Obj_subIndex_t',['../struct_obj__sub_index__t.html',1,'']]],
+  ['obj_5fwrite_18',['obj_write',['../object__control_8c.html#aced665d65fe5fea30e5611b1efb99bf5',1,'obj_write(Obj_control_t *control, uint16_t index, uint8_t subIndex):&#160;object_control.c'],['../object__control_8h.html#aced665d65fe5fea30e5611b1efb99bf5',1,'obj_write(Obj_control_t *control, uint16_t index, uint8_t subIndex):&#160;object_control.c']]],
+  ['object_5fcontrol_2ec_19',['object_control.c',['../object__control_8c.html',1,'']]],
+  ['object_5fcontrol_2eh_20',['object_control.h',['../object__control_8h.html',1,'']]],
+  ['objects_21',['objects',['../objects_8c.html#aa4483f4b60afc6ac101298579efe3ec9',1,'objects.c']]],
+  ['objects_2ec_22',['objects.c',['../objects_8c.html',1,'']]],
+  ['objects_2eh_23',['objects.h',['../objects_8h.html',1,'']]],
+  ['objects_5ft_24',['objects_t',['../structobjects__t.html',1,'']]]
+];
