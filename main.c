e6c8d220e23e58636945b53cd2204528914df0de
Mehmet Umut Boran <mboran16@ku.edu.tr>
2020-08-29 14:26:13 +0300

    Made some code refinements and added test cases

--- main.c
+++ main.c
@@ -19,14 +19,16 @@ int main()
 
     /**** Read NullParam Error ****/
     err = obj_read(control, 0x1000, 3);
-    if (err != OBJ_ERR_SUCCESS) printf("Read error: %d\n", err);
+    if (err == OBJ_ERR_SUCCESS){
+        printf("Successfully read: %X\n", Obj_getUint32(control->obj_arg.databuffer));
+    } else printf("Read error: %d\n", err);
 
     control = &obj_control;
 
     control->obj_index = index_objects;
     control->nof_index = NOF_INDEX_ARRAY;
 
-    /**** Successful Write ****/
+    /**** Successful Write #1 ****/
     uint32_t data = 0x12345678;
     Obj_setUint32(control->databuffer, data);
 
@@ -39,27 +41,40 @@ int main()
                Obj_getUint32(control->obj_arg.databuffer));
     } else printf("Write data error: %d\n", err);
 
-    /**** Write Index Error ****/
-    uint32_t data2 = 0x11111111;
+    /**** Successful Write #2 ****/
+    uint8_t data2 = 0x24;
     Obj_setUint32(control->databuffer, data2);
+
+    err = obj_write(control, 0x1001, 2);
+
+    if (err == OBJ_ERR_SUCCESS){
+        printf("Write successful to Index:0x%X subIndex:0x%X data:0x%X\n",
+               control->obj_arg.index,
+               control->obj_arg.subIndex,
+               Obj_getUint32(control->obj_arg.databuffer));
+    } else printf("Write data error: %d\n", err);
+
+    /**** Write Index Error ****/
+    uint32_t data3 = 0x11111111;
+    Obj_setUint32(control->databuffer, data3);
 
     err = obj_write(control, 0x1004, 0);
 
     if (err == OBJ_ERR_SUCCESS){
-        printf("Index:0x%X subIndex:0x%X data:0x%X\n",
+        printf("Write successful to Index:0x%X subIndex:0x%X data:0x%X\n",
                control->obj_arg.index,
                control->obj_arg.subIndex,
                Obj_getUint32(control->obj_arg.databuffer));
     } else printf("Write data error: %d\n", err);
 
     /**** Write Readonly Error ****/
-    uint32_t data3 = 0x87654321;
-    Obj_setUint32(control->databuffer, data3);
+    uint32_t data4 = 0x87654321;
+    Obj_setUint32(control->databuffer, data4);
 
     err = obj_write(control, 0x1003, 0);
 
     if (err == OBJ_ERR_SUCCESS){
-        printf("Index:0x%X subIndex:0x%X data:0x%X\n",
+        printf("Write successful to Index:0x%X subIndex:0x%X data:0x%X\n",
                control->obj_arg.index,
                control->obj_arg.subIndex,
                Obj_getUint32(control->obj_arg.databuffer));
@@ -71,14 +86,23 @@ int main()
         printf("Successfully read: %X\n", Obj_getUint32(control->obj_arg.databuffer));
     } else printf("Read error: %d\n", err);
 
+    /**** Successful Read #2 ****/
+    err = obj_read(control, 0x1001, 2);
+    if (err == OBJ_ERR_SUCCESS){
+        printf("Successfully read: %X\n", Obj_getUint32(control->obj_arg.databuffer));
+    } else printf("Read error: %d\n", err);
 
     /**** Read SubIndex Error ****/
     err = obj_read(control, 0x1001, 6);
-    if (err != OBJ_ERR_SUCCESS) printf("Read error: %d\n", err);
+    if (err == OBJ_ERR_SUCCESS){
+        printf("Successfully read: %X\n", Obj_getUint32(control->obj_arg.databuffer));
+    } else printf("Read error: %d\n", err);
 
-    /**** Read Writeonly error ****/
+    /**** Read Writeonly Error ****/
     err = obj_read(control, 0x2002, 0);
-    if (err != OBJ_ERR_SUCCESS) printf("Read error: %d\n", err);
+    if (err == OBJ_ERR_SUCCESS){
+        printf("Successfully read: %X\n", Obj_getUint32(control->obj_arg.databuffer));
+    } else printf("Read error: %d\n", err);
 
     return 0;
 }
